f29dbe60806b36300ca5535214c9cdc886cfa962
yefan <45190579@qq.com>
2026-03-09 15:21:09 +0800

    feat: demo

--- apps/Sub/src/App.vue
+++ apps/Sub/src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { MyInput, MySelect } from "@monorepo/components";
+import { add, toUpperCase } from "@monorepo/utils";
+
+const sum = add(5, 3);
+const upperCaseText = toUpperCase("hello world");
 </script>
 
 <template>
@@ -7,6 +11,10 @@ import { MyInput, MySelect } from "@monorepo/components";
     <h1>Testing Components</h1>
     <MyInput />
     <MySelect />
+    <div>
+      <p>5 + 3 = {{ sum }}</p>
+      <p>Uppercase: {{ upperCaseText }}</p>
+    </div>
   </div>
 </template>
 
